b450545b7daa439959d4e490a6bcc89f5c1d9e0d
Joost Flick <joostflick@gmail.com>
2019-02-27 15:02:17 +0100

    Random colors + mouse

--- week1/app.js
+++ week1/app.js
@@ -2,6 +2,10 @@ var body = document.getElementsByTagName('body')[0]
 var content = document.getElementsByClassName('content')[0]
 var articleSmall1 = document.querySelectorAll('.content__article--small')
 var articleBig = document.querySelectorAll('.content__article--big')
+var mouse = document
+  .getElementsByClassName('mouse')[0]
+  .contentDocument.getElementById('Capa_1')
+console.log(mouse)
 
 function Switch() {
   positionSwitch()
@@ -36,6 +40,7 @@ function positionSwitch() {
 
 function lightSwitch() {
   if (content.className.includes('dark')) {
+    mouse.style.setProperty('fill', 'initial')
     content.classList.add('light')
     content.classList.remove('dark')
     body.classList.remove('dark')
@@ -48,6 +53,7 @@ function lightSwitch() {
       article.classList.remove('dark')
     })
   } else {
+    mouse.style.setProperty('fill', 'white')
     content.classList.add('dark')
     content.classList.remove('light')
     body.classList.add('dark')
@@ -61,3 +67,39 @@ function lightSwitch() {
     })
   }
 }
+
+let root = document.documentElement
+
+root.addEventListener('mousemove', e => {
+  root.style.setProperty('--mouse-x', e.clientX + 'px')
+  root.style.setProperty('--mouse-y', e.clientY + 'px')
+})
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+function getSemiRandomColor() {
+  var colors = [
+    '#C2D5A5',
+    '#2F287A',
+    '#A13B71',
+    '#034458',
+    '#70EF31',
+    '#ECD576',
+    '#1B4ED3',
+    '#9F4B53',
+    '#B9EDF8',
+    '#F7BF72',
+    '#5BDC7F'
+  ]
+
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+root.style.setProperty('--main-color', getSemiRandomColor())
